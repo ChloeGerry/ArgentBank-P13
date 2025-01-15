fdafb72847edc39587489d1e7d3ec4044138ae39
Chloé Gerry <cgerry@corp.free.fr>
2025-01-15 18:39:27 +0100

    feat: add colors in tailwind config file

--- frontend/tailwind.config.ts
+++ frontend/tailwind.config.ts
@@ -2,7 +2,13 @@
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        purple: "#6458F6",
+        lightGrey: "#E0E6EE",
+        darkGrey: "#2c3e50",
+      },
+    },
   },
   plugins: [],
 };
