04a9b9bd41660fce7491ff4cc1f557b7e30d89b2
Chloé Gerry <cgerry@corp.free.fr>
2025-01-15 18:43:30 +0100

    feat: create footer component

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -2,12 +2,14 @@ import { StrictMode } from "react";
 import { BrowserRouter } from "react-router-dom";
 import { createRoot } from "react-dom/client";
 import Routing from "./Routing.tsx";
+import Footer from "./components/layout/Footer/index.tsx";
 import "./index.css";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <BrowserRouter>
       <Routing />
+      <Footer />
     </BrowserRouter>
   </StrictMode>
 );
