9e52ca0f7113bab08c05d8e06e5e66c5705f53d1
Chloé Gerry <cgerry@corp.free.fr>
2025-02-24 15:12:07 +0100

    feat: create signup and transactions routes

--- src/components/Routing/index.tsx
+++ src/components/Routing/index.tsx
@@ -3,12 +3,16 @@ import Dashboard from "@/pages/Dashboard";
 import Home from "@/pages/Home";
 import Login from "@/pages/Login";
 import Error from "@/pages/Error";
+import Signup from "@/pages/Signup";
+import Transactions from "@/pages/Transactions";
 
 const Routing = () => (
   <Routes>
     <Route path="/" element={<Home />} />
     <Route path="/login" element={<Login />} />
     <Route path="/dashboard/:id" element={<Dashboard />} />
+    <Route path="/signup" element={<Signup />} />
+    <Route path="/dashboard/:id/accounts/:accountId/transactions" element={<Transactions />} />
     <Route path="/*" element={<Error />} />
   </Routes>
 );
